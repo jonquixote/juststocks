9f0e32a6d7443d29ac1ec4d0f1f65090d131f494
“jonuixote” <jehawley@gmail.com>
2020-04-17 05:46:00 -0700

    fallback

--- config/application.rb
+++ config/application.rb
@@ -24,10 +24,6 @@ module Juststocks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    def fallback_index_html
-      render :file => 'client/build/index.html'
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
