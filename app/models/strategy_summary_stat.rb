83bd818cb50a60261765ed1f825692275e18a28d
“jonuixote” <jehawley@gmail.com>
2020-04-24 19:04:36 -0700

    fix summary

--- app/models/strategy_summary_stat.rb
+++ app/models/strategy_summary_stat.rb
@@ -2,5 +2,5 @@ class StrategySummaryStat < ApplicationRecord
   extend FriendlyId
   friendly_id :strategy_name, use: :slugged
   belongs_to :strategy
-  default_scope -> { order(created_at: :asc) }
+  default_scope -> { order(created_at: :desc) }
 end
